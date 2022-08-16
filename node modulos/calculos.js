8918e4caa1a76e461a4f9f59f7b4e94d8b0f4190
crohnaro <viniciosalfa@hotmail.com>
2022-08-16 13:38:52 -0300

    subindo praticas com node.js

---
+++ node modulos/calculos.js
@@ -0,0 +1,12 @@
+function soma (n1, n2){
+    return n1 + n2
+}
+
+function multiplicative (n1, n2){
+    return n1 * n2
+}
+
+module.exports = {
+    soma,
+    multiplicative
+}
---
+++ node modulos/nodemodules.js
@@ -0,0 +1,6 @@
+//desestructuring  
+const {soma, multiplicative} = require ('./calculos.js')
+
+const resultado = soma(10,20)
+
+console.log(resultado)
