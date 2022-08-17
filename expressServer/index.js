4b83f05e850efcf4f4ea323919e581a215a45fe2
crohnaro <viniciosalfa@hotmail.com>
2022-08-17 16:46:40 -0300

    usando loops em ejs

--- expressServer/index.js
+++ expressServer/index.js
@@ -1,33 +1,51 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 //definindo template engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Definindo arquivos estáticos
 // app.use(express.static(path.join(__dirname, 'views')))
 
 // Definindo arquivos public
-app.use(express.static(path.join(__dirname, 'public')))
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // rotas
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    tittle: "Teste A",
+  });
+});
 
-app.get('/posts', (req, res) => {
-    res.render('posts')
-})
+app.get("/posts", (req, res) => {
+  res.render("posts", {
+    tittle: "Teste B",
+    posts: [
+        {
+            title: 'Testeabc',
+            text: 'Lorem ipsum dolor sit amet, consectet'
+        },
+        {
+            title: 'Testeaasdbc',
+            text: 'Lorem ipsum dolor sit amet, consectet'
+        },
+        {
+            title: 'Testeaasdbc',
+            text: 'Lorem ipsum dolor sit amet, consectet'
+        },
+    ]
+  });
+});
 
 //404 error
-app.use((req, res) => { //middleware
-    res.send('Pagina nao encontrada')
-})
+app.use((req, res) => {
+  //middleware
+  res.send("Pagina nao encontrada");
+});
 
 //executando o server
-const port = process.env.PORT || 8080
-app.listen(port, () =>{ console.log('listening on')
-})
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("listening on");
+});
